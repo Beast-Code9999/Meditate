33a569fae71c597adbd7bae335d04f78db729886
Beast Code <beastcode9999@gmail.com>
2023-01-25 22:10:37 +0900

    modified .container && .instruction-text

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// starts with an exhale 
+// 6 breathing in 
+// inhale 5 seconds
+// exhale 5 seconds
+
+// after click text: "Bring awareness to your breath" during breathe out
+// breathe in
+// breathe out
+
 // footer script
 function addDynamicFooterDate() {
     const footer = document.querySelector('.footer__year');
